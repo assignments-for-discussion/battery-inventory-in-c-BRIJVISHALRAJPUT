2a26debfb8233d6aa10a864f555acd2dc2652d57
BRIJVISHALRAJPUT <112466156+BRIJVISHALRAJPUT@users.noreply.github.com>
2022-09-02 19:10:11 +0530

    Update bucketer.c

--- bucketer.c
+++ bucketer.c
@@ -8,7 +8,23 @@ struct CountsByUsage {
 };
 
 struct CountsByUsage countBatteriesByUsage(const int* cycles, int nBatteries) {
-  struct CountsByUsage counts = {2, 3, 1};
+  struct CountsByUsage counts = {0, 0, 0};
+  int count1=0;
+  int count2=0;
+  int count3=0;
+  for(int i=0;i<nBatteries;i++)
+  {
+  if(cycles[i]<400)
+    count1++;
+  else if(cycles[i]>=400&&cycles[i]<=919) 
+    count2++;
+  else if(cycles[i]>=920)
+    count3++;
+    
+  }
+  counts[0]=count1;
+  counts[1]=count2;
+  counts[3]=count3;
   return counts;
 }
 
